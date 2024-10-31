6c5990333a1bfaac20e7f9675b3beef755b1d7ed
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-10-31 18:43:57 +0600

    отредактировала приложение, отформатировала с помощью prettier.

--- src/containers/EditPageContainer/EditPageContainer.tsx
+++ src/containers/EditPageContainer/EditPageContainer.tsx
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { IPage } from "../../types";
 import { useNavigate, useParams } from "react-router-dom";
 import Admin from "../../containers/Admin/Admin.tsx";
 import axiosApi from "../../../axiosAPI.ts";
 
 const EditPageContainer = () => {
-    const { pageId } = useParams<{ pageId: string }>();
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState<IPage>({
-        title: '',
-        content: '',
-    });
+  const { pageId } = useParams<{ pageId: string }>();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<IPage>({
+    title: "",
+    content: "",
+  });
 
-    useEffect(() => {
-        const fetchPageContent = async () => {
-            try {
-                const response = await axiosApi.get(`/pages/${pageId}.json`);
-                if (response.data) {
-                    setFormData(response.data);
-                }
-            } catch (error) {
-                console.error("Error fetching page content:", error);
-            }
-        };
-
-        if (pageId) {
-            void fetchPageContent();
+  useEffect(() => {
+    const fetchPageContent = async () => {
+      try {
+        const response = await axiosApi.get(`/pages/${pageId}.json`);
+        if (response.data) {
+          setFormData(response.data);
         }
-    }, [pageId]);
-
-    const handleFormChange = (newForm: IPage) => {
-        setFormData(newForm);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    const handleSave = async () => {
-        try {
-            await axiosApi.put(`/pages/${pageId}`, formData);
-            navigate(`/pages/${pageId}`);
-        } catch (error) {
-            console.error("Error saving page:", error);
-        }
-    };
+    if (pageId) {
+      void fetchPageContent();
+    }
+  }, [pageId]);
 
-    return (
-        <Admin
-            form={formData}
-            onFormChange={handleFormChange}
-            onSave={handleSave}
-        />
-    );
+  const handleFormChange = (newForm: IPage) => {
+    setFormData(newForm);
+  };
+
+  const handleSave = async () => {
+    try {
+      await axiosApi.put(`/pages/${pageId}`, formData);
+      navigate(`/pages/${pageId}`);
+    } catch (error) {
+      console.error("Error saving page:", error);
+    }
+  };
+
+  return (
+    <Admin
+      form={formData}
+      onFormChange={handleFormChange}
+      onSave={handleSave}
+    />
+  );
 };
 
 export default EditPageContainer;
